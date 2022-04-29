3cbd118d493cbb907dd5a24303644e147a03d19d
DESKTOP-5647LPU\User <fil.piech@gmail.com>
2022-04-29 22:31:15 +0200

    add: full working tests

--- tests.py
+++ tests.py
@@ -44,38 +44,62 @@ TEST_PART_UPDATE = {
     "depositpaid" : True,
 }
 
+final_result = True
+
 try:
     api = apimap.ApiMap()
     print('Create session and getting token: OK')
 except:
     print('Create session and getting token: ERROR')
+    final_result = False
 
 try:
     id = api.get_booking_ids()[0]
     print(f'Get id: OK ({id})')
 except:
     print('Get id: ERROR')
+    final_result = False
 
 try:
     api.get_booking(id)
     print(f'Get booking by id: OK')
-    print(f' ->{api.get_booking(id)}')
 except:
     print('Get booking by id: ERROR')
+    final_result = False
 
 try:
     api.update_booking(id, TEST_UPDATE)
-    print(f'Update booking: OK')
-    print(f' ->{api.get_booking(id)}')
+
+    comp_update = True
+    for key in TEST_UPDATE.keys():
+        if TEST_UPDATE[key] != api.get_booking(id)[key]:
+            comp_update = False
+    
+    if comp_update:
+        print(f'Update booking: OK')
+    else:
+        print(f'Update booking: ERROR')
+        final_result = False
 except:
     print(f'Update booking: ERROR')
+    final_result = False
 
 try:
     api.partial_update_booking(id, TEST_PART_UPDATE)
-    print(f'Partial update booking: OK')
-    print(f' ->{api.get_booking(id)}')
+
+    comp_part_update = True
+    for key in TEST_PART_UPDATE.keys():
+        if TEST_UPDATE[key] != api.get_booking(id)[key]:
+            comp_update = False
+    
+    if comp_part_update == True:
+        print(f'Partial update booking: OK')
+    else:
+        print(f'Partial update booking: ERROR')
+        final_result = False
 except:
     print('Partial update booking: ERROR')
+    final_result = False
 
 try:
     api.delete_booking(id)
@@ -83,3 +107,26 @@ try:
         print(f'Delete booking: OK')
 except:
     print('Delete booking: ERROR')
+    final_result = False
+
+try:
+    id_create = api.create_booking(TEST_CREATE)
+
+    comp_create = True
+    for key in TEST_CREATE.keys():
+        if TEST_CREATE[key] != api.get_booking(id_create)[key]:
+            comp_create = False
+    
+    if comp_create:
+        print(f'Create booking: OK')
+    else:
+        print(f'Create booking: ERROR')
+        final_result = False
+except:
+    print('Create booking: ERROR')
+    final_result = False
+
+if final_result:
+    print('Final result: OK')
+else:
+    print('Final result: ERROR(s) occurred')
