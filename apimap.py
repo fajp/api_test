56e796f11203a0a342e108e34514e0c1cdf0ba88
DESKTOP-5647LPU\User <fil.piech@gmail.com>
2022-04-29 21:29:59 +0200

    add: working ApiMap class

--- apimap.py
+++ apimap.py
@@ -1,7 +1,8 @@
 import requests
+import json
 
 
-class apiMap:
+class ApiMap:
 
     config = {
         'token': {
@@ -12,38 +13,108 @@ class apiMap:
                 'username' : 'admin',
                 'password' : 'password123'
             }
+        },
+        'bookings_ids': {
+            'base-url': 'https://restful-booker.herokuapp.com/booking',
+            'filters': {
+                'first_name': 'firstname',
+                'last_name': 'lastname',
+                'check_in': 'checkin',
+                'check_out': 'checkout'
+            }
         }
-
     }
 
 
-    def __init__(self, username: str = "admin", password: str = "password123"):        
-        apiMap.config['token']['data']['username'] = username
-        apiMap.config['token']['data']['password'] = password
-
+    def __init__(self, username: str = "admin",
+                password: str = "password123"):
         self.username = username
         self.password = password
-        self.session = self._get_session()
+        self.session = requests.Session()
         self.token = self._get_token()
 
 
-    def _get_session(self) -> str:
-        session = requests.Session()
-        return session.post(
-            url=self.config['token']['url'],
-            # headers=self.config['token']['headers'],
-            data=self.config['token']['data'],
-        )
-
     def _get_token(self) -> str:
-        session = requests.Session()
-        response = session.post(
-            url=self.config['token']['url'],
-            # headers=self.config['token']['headers'],
-            data=self.config['token']['data'],
+        response = self.session.post(
+            url="https://restful-booker.herokuapp.com/auth",
+            data={
+                'username': self.username,
+                'password': self.password,
+            }
         )
         return response.json()['token']
 
 
-test = apiMap()
-print(test.token)
+    def get_booking_ids(self, filters: dict = {}) -> list:
+        """Returns a list of bookings IDs for the given filters"""
+
+        url = "https://restful-booker.herokuapp.com/booking"
+
+        if bool(filters):
+            url += "?"
+            if 'firstname' in filters.keys():
+                url += f"firstname={filters['firstname']}"
+            if 'lastname' in filters.keys():
+                url += f"&lastname={filters['lastname']}"
+
+        response = self.session.get(url)
+        return [id['bookingid'] for id in response.json()]
+
+    
+    def get_booking(self, id: int) -> dict:
+        """Returns booking information by given id"""
+
+        return self.session.get(
+            f"https://restful-booker.herokuapp.com/booking/{id}"
+        ).json()
+
+
+    def create_booking(self, data: dict) -> int:
+        """Returns id after creating booking"""
+
+        response = self.session.post(
+            url="https://restful-booker.herokuapp.com/booking",
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(data),
+        )
+        return response.json()['bookingid']
+
+    
+    def update_booking(self, id: int, data: dict):
+        """Updates booking"""
+
+        response = self.session.put(
+            url=f"https://restful-booker.herokuapp.com/booking/{id}",
+            headers={
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Cookie': f'token={self.token}',
+            },
+            data=json.dumps(data)
+        )
+
+
+    def partial_update_booking(self, id: int, data: dict):
+        """Partialy updates booking"""
+
+        response = self.session.patch(
+            url=f"https://restful-booker.herokuapp.com/booking/{id}",
+            headers={
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Cookie': f'token={self.token}',
+            },
+            data=json.dumps(data)
+        )
+
+
+    def delete_booking(self, id):
+        """Delete booking by given id"""
+
+        response = self.session.delete(
+            url=f"https://restful-booker.herokuapp.com/booking/{id}",
+            headers={
+                'Content-Type': 'application/json',
+                'Cookie': f'token={self.token}',
+            },
+        )
--- tests.py
+++ tests.py
@@ -1,17 +1,35 @@
 import requests
-
-session = requests.Session()
-response = session.post(
-    url="https://restful-booker.herokuapp.com/auth",
-    data={
-        "username" : "admin",
-        "password" : "password123"
-    }
-)
-print(response.json())
+import apimap
 
 
-session = requests.Session()
-response = session.get("https://restful-booker.herokuapp.com/booking")
-print(response.json())
+data_ = {
+    "firstname" : "Jim",
+    "lastname" : "Neutron",
+    "totalprice" : 111,
+    "depositpaid" : 'true',
+    "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+    },
+    "additionalneeds" : "Breakfast"
+}
+data_2 = {
+    "firstname" : "Jim",
+    "lastname" : "TEST!",
+    "totalprice" : 111,
+    "depositpaid" : 'true',
+    "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+    },
+    "additionalneeds" : "Breakfast"
+}
 
+api = apimap.ApiMap()
+id = api.create_booking(data_)
+print(api.get_booking(id))
+api.update_booking(id, data_2)
+print(api.get_booking(id))
+print(id)
+api.delete_booking(id)
+print(api.get_booking(id))
